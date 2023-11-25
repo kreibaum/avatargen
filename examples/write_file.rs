df06c63e9ac512b3421426c595f68ad0218f90ec
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-11-25 20:35:24 +0100

    Use indexed colors with alpha channel

--- examples/write_file.rs
+++ examples/write_file.rs
@@ -10,10 +10,13 @@ fn main() {
     let w = BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, 2, 1); // Width is 2 pixels and height is 1.
-    encoder.set_color(png::ColorType::Rgba);
+    encoder.set_color(png::ColorType::Indexed);
     encoder.set_depth(png::BitDepth::Eight);
+    let palette = [255, 0, 0, 255, 0, 0];
+    encoder.set_palette(palette.as_ref());
+    encoder.set_trns([255, 100].as_ref());
     let mut writer = encoder.write_header().unwrap();
 
-    let data = [255, 0, 0, 255, 0, 0, 0, 255]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
+    let data = [0, 1]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
     writer.write_image_data(&data).unwrap(); // Save
 }
